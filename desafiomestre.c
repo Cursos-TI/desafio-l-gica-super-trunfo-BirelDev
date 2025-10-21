81a4b7ea54866e9e2165a15b36d66b477d22d4a7
BirelDev <bielg3148@gmail.com>
2025-10-20 22:04:08 -0300

    desafioMestre update

--- desafiomestre.c
+++ desafiomestre.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
+
+
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil.1252");
     //variaveis carta 1
+    float categoriasoma1a = 0;
+    float categoriasoma1b = 0;
+    float resultadofinalcarta1 = 0;
     char estadoCarta1[10];
     char codigoCarta1[10];
     char nomeCarta1[30];
@@ -28,6 +33,9 @@ int main() {
     pibpercapita1 = (pibCarta1 * 1000000000) / populacaoCarta1; // cálculo automático
  
     //variaveis carta 2
+    float categoriasoma2a = 0;
+    float categoriasoma2b = 0;
+    float resultadofinalcarta2 = 0;
     char estadoCarta2[10];
     char codigoCarta2[10];
     char nomeCarta2[30];
@@ -114,6 +122,9 @@ int main() {
                     switch (categoria1)     
                     {
                     case 1:
+                        categoriasoma1a += populacaoCarta1;
+                        categoriasoma1b += populacaoCarta2;
+
                         if (populacaoCarta1 > populacaoCarta2){
                             printf("%s venceu %s na categoria população\n",nomeCarta1, nomeCarta2);
                         } else if (populacaoCarta1 < populacaoCarta2){
@@ -121,9 +132,11 @@ int main() {
                         } else {
                             printf("As cartas empataram na categoria população\n");
                         }
-                        
+                        break;
                         
                     case 2:
+                        categoriasoma1a += areaCarta1;
+                        categoriasoma1b += areaCarta2;
                         if (areaCarta1 > areaCarta2){
                             printf("%s venceu %s na categoria área\n", nomeCarta1, nomeCarta2);
                         } else if (areaCarta1 < areaCarta2){
@@ -134,6 +147,8 @@ int main() {
                         break;
                         
                     case 3:
+                        categoriasoma1a += pibCarta1;
+                        categoriasoma1b += pibCarta2;
                         if (pibCarta1 > pibCarta2){
                             printf("%s venceu %s na categoria PIB\n", nomeCarta1, nomeCarta2);
                         } else if (pibCarta1 < pibCarta2){
@@ -144,16 +159,20 @@ int main() {
                         break;
                         
                     case 4:
+                        categoriasoma1a += pontoTuristicosCarta1;
+                        categoriasoma1b += pontoTuristicosCarta2;
                         if (pontoTuristicosCarta1 > pontoTuristicosCarta2){
                             printf("%s venceu %s na categoria pontos turísticos\n", nomeCarta1, nomeCarta2);
                         } else if (pontoTuristicosCarta1 < pontoTuristicosCarta2){
-                            printf("%s venceu %S na categoria pontos turísticos\n", nomeCarta2, nomeCarta1);
+                            printf("%s venceu %s na categoria pontos turísticos\n", nomeCarta2, nomeCarta1);
                         } else {
                             printf("As cartas empataram na categoria pontos turísticos\n");
                         }
                         break;
                         
                     case 5:
+                        categoriasoma1a += densidadepopulacional1;
+                        categoriasoma1b += densidadepopulacional2;
                         if (densidadepopulacional1 < densidadepopulacional2){
                             printf("%s venceu %s na categoria densidade demográfica\n", nomeCarta1, nomeCarta2);
                         } else if (densidadepopulacional1 > densidadepopulacional2){
@@ -164,8 +183,14 @@ int main() {
                         }  
                         break;
 
-                    };
-
+                     default:
+                        printf("Categoria inválida! Por favor, escolha uma categoria entre 1 e 5.\n");
+                        
+                        return 0;   
+                    }
+                    
+                    printf("--------------------- \n");
+                    //menu de comparação
                     printf("Escolha a segunda categoria para comparar as cartas: \n");
                     printf("1 - População \n");
                     printf("2 - Área \n");
@@ -173,19 +198,19 @@ int main() {
                     printf("4 - Pontos turísticos \n");
                     printf("5 - Densidade demográfica \n");
 
-                    
                     int categoria2;
-                    printf("Digite o número da segunda categoria escolhida: "); 
+                    printf("Digite o número da categoria escolhida: ");
                     scanf("%d", &categoria2);
-                    printf("Escolha registrada!\n");
-
-                    if (categoria1 == categoria2) {
-                        printf("Você já escolheu essa categoria. Por favor, escolha uma diferente.\n");
-                    } else {
-                        
-                    switch (categoria2)     
+                    printf("Iniciando comparção segunda categoria!\n");
+                    if (categoria2 == categoria1)
                     {
-                    case 1:
+                        printf("Você já escolheu essa categoria, por favor escolha outra!\n");
+                    } else{
+                        switch (categoria2)
+                        {
+                     case 1:
+                        categoriasoma2a += densidadepopulacional1;
+                        categoriasoma2b += densidadepopulacional2;
                         if (populacaoCarta1 > populacaoCarta2){
                             printf("%s venceu %s na categoria população\n",nomeCarta1, nomeCarta2);
                         } else if (populacaoCarta1 < populacaoCarta2){
@@ -196,6 +221,8 @@ int main() {
                         break;
                         
                     case 2:
+                        categoriasoma2a += densidadepopulacional1;
+                        categoriasoma2b += densidadepopulacional2;
                         if (areaCarta1 > areaCarta2){
                             printf("%s venceu %s na categoria área\n", nomeCarta1, nomeCarta2);
                         } else if (areaCarta1 < areaCarta2){
@@ -206,6 +233,8 @@ int main() {
                         break;
                         
                     case 3:
+                        categoriasoma2a += densidadepopulacional1;
+                        categoriasoma2b += densidadepopulacional2;
                         if (pibCarta1 > pibCarta2){
                             printf("%s venceu %s na categoria PIB\n", nomeCarta1, nomeCarta2);
                         } else if (pibCarta1 < pibCarta2){
@@ -216,16 +245,20 @@ int main() {
                         break;
                         
                     case 4:
+                        categoriasoma2a += densidadepopulacional1;
+                        categoriasoma2b += densidadepopulacional2;
                         if (pontoTuristicosCarta1 > pontoTuristicosCarta2){
                             printf("%s venceu %s na categoria pontos turísticos\n", nomeCarta1, nomeCarta2);
                         } else if (pontoTuristicosCarta1 < pontoTuristicosCarta2){
-                            printf("%s venceu %S na categoria pontos turísticos\n", nomeCarta2, nomeCarta1);
+                            printf("%s venceu %s na categoria pontos turísticos\n", nomeCarta2, nomeCarta1);
                         } else {
                             printf("As cartas empataram na categoria pontos turísticos\n");
                         }
                         break;
                         
                     case 5:
+                        categoriasoma2a += densidadepopulacional1;
+                        categoriasoma2b += densidadepopulacional2;
                         if (densidadepopulacional1 < densidadepopulacional2){
                             printf("%s venceu %s na categoria densidade demográfica\n", nomeCarta1, nomeCarta2);
                         } else if (densidadepopulacional1 > densidadepopulacional2){
@@ -235,13 +268,31 @@ int main() {
                             
                         }  
                         break;
-                    }
-                   return 0;
+                        default:
+                        printf("Categoria inválida! Por favor, escolha uma categoria entre 1 e 5.\n");
+                        return 0;
+                        }
                     
-                }
+                        printf("-------------------------------------\n");
+                        printf("Resultado final da comparação entre as cartas:\n");
+                        printf("Comparação entre %s e %s:\n", nomeCarta1, nomeCarta2);
+                        resultadofinalcarta1 += (categoriasoma1a + categoriasoma2a);
+                        resultadofinalcarta2 += (categoriasoma1b + categoriasoma2b);
+                        printf("Soma das categorias de %s: %.2f\n", nomeCarta1, resultadofinalcarta1);
+                        printf("Soma das categorias de %s: %.2f\n", nomeCarta2, resultadofinalcarta2);
+                        if (resultadofinalcarta1 > resultadofinalcarta2){
+                            printf("%s é a carta vencedora!\n", nomeCarta1);
+                        } else if (resultadofinalcarta1 < resultadofinalcarta2){
+                            printf("%s é a carta vencedora!\n", nomeCarta2);
+                        } else {
+                            printf("As cartas empataram na comparação final!\n");
+                        }
+                        
 
+                    }    return 0;
+}
                 
 
-   
+        
 
-    
+                
